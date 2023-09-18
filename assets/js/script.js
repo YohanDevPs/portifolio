e794d400d919ce79061f61dd2dddd8b46d8e6a42
Yohan Silva <yohanps.dev@gmail.com>
2023-09-18 17:40:09 -0300

    improve button style and show correct section

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let navLinks = document.querySelectorAll("nav ul li a");
+
+navLinks.forEach((item) => {
+  console.log(item);
+  item.addEventListener("click", function () {
+    document.querySelector("nav ul li a.active").classList.remove("active");
+    item.classList.add("active");
+    document.querySelector("main > section.active").classList.remove("active");
+    document
+      .querySelector(`main > section${item.getAttribute("href")}`)
+      .classList.add("active");
+    console.log(`main > section${item.getAttribute("href")}`);
+  });
+});
